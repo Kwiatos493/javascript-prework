50e57a70db56b2296af266d39b68739e7e9e1e3d
Krystian Kwiatos <matius4321@gmail.com>
2021-10-02 13:14:49 +0200

    próba poprawy zadania

--- js/script.js
+++ js/script.js
@@ -9,15 +9,12 @@ let computerMove = 'nieznany ruch';
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
-if(randomNumber == 2){
+else if(randomNumber == 2){
     computerMove = 'papier';
 }
-if(randomNumber == 3){
+else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
-
-printMessage('Mój ruch to: ' + computerMove);
-
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -27,37 +24,40 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-if(playerInput == '2'){
+}
+else if(playerInput == '2'){
   playerMove = 'papier';
-if(playerInput == '3'){
+}
+else if (playerInput == '3'){
   playerMove = 'nożyce';
 }
 
 if( computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
   }
-if( computerMove == 'papier' && playerMove == 'kamień'){
+else if( computerMove == 'papier' && playerMove == 'kamień'){
     printMessage('Przegrywasz!');
   }  
-if( computerMove == 'nożyce' && playerMove == 'kamień'){
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
     printMessage('Ty wygrywasz!');
   }
-if( computerMove == 'kamień' && playerMove == 'nożyce'){
+else if ( computerMove == 'kamień' && playerMove == 'nożyce'){
     printMessage('Przegrywasz!');
   }
-if( computerMove == 'nożyce' && playerMove == 'papier'){
+else if( computerMove == 'nożyce' && playerMove == 'papier'){
     printMessage('Przegrywasz!');
   }
-if( computerMove == 'papier' && playerMove == 'nożyce'){
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
     printMessage('Ty wygrywasz!');
   }  
-if( computerMove == 'kamień' && playerMove == 'kamień'){
+else if( computerMove == 'kamień' && playerMove == 'kamień'){
     printMessage('Remis');
   }
-if( computerMove == 'papier' && playerMove == 'papier'){
+else if( computerMove == 'papier' && playerMove == 'papier'){
     printMessage('Remis');
   }  
-if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
     printMessage('Remis');
   }
+
 printMessage('Twój ruch to: ' + playerMove);
