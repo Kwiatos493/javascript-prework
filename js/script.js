a207601ce2612a5128f38c3c86df2c885751560b
Krystian Kwiatos <matius4321@gmail.com>
2021-10-05 08:33:46 +0200

    poprawa zadanie nr. 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 let computerMove = getMoveName(randomNumber);
 
+/*
 if(randomNumber == 1){
   computerMove = 'kamień';
 }
@@ -10,13 +11,15 @@ else if(randomNumber == 2){
 else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
+*/
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove =  getMoveName(randomNumber);
+let playerMove =  getMoveName(playerInput);
 
+/*
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -26,7 +29,7 @@ else if(playerInput == '2'){
 else if (playerInput == '3'){
   playerMove = 'nożyce';
 }
-
+*/
 
 if( computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
